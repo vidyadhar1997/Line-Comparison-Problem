0e30654d94d7dc45b3115f23a9b6c02b429adf6f
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-02 17:57:40 +0530

    [vidyadhar hudge]Add:To check equality of two lines

--- LineComparisonProblems/Program.cs
+++ LineComparisonProblems/Program.cs
@@ -6,12 +6,21 @@ namespace LineComparisonProblems
     {
         static void Main(string[] args)
         {
+            int x1, x2, y1, y2;
             Console.WriteLine("Enter the value of (x1,y1),(x2,y2) cordinates");
-            int x1=Convert.ToInt32(Console.ReadLine());
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            int y1 = Convert.ToInt32(Console.ReadLine());
-            int y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(toFindLength(x1, y1, x2, y2));
+            x1=Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            int length1=toFindLength(x1, y1, x2, y2);
+
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            int length2 = toFindLength(x1, y1, x2, y2);
+
+            Console.WriteLine(length1.Equals(length2));
 
         }
 
