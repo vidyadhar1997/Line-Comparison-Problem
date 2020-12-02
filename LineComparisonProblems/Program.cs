7f66bae2c682692179433e69ad420d7d73c8e8ff
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-02 17:44:36 +0530

    [vidyadhar hudge]Add:To find length of co-ordinates

--- LineComparisonProblems/Program.cs
+++ LineComparisonProblems/Program.cs
@@ -6,7 +6,18 @@ namespace LineComparisonProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to line comparison problme");
+            Console.WriteLine("Enter the value of (x1,y1),(x2,y2) cordinates");
+            int x1=Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(toFindLength(x1, y1, x2, y2));
+
+        }
+
+        private static int toFindLength(int x1, int y1, int x2, int y2)
+        {
+            return(int) Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         }
     }
 }
